d9d05c828a99bdac62a75baef87274ad0f112a7b
ADS 2021.1N <36124872021.1N@senacrjedu.local>
2022-09-14 20:06:49 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { List, ProgressBar } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <List.Item
+        title="First Item"
+        description="Item description"
+        left={props => <List.Icon color={'#ff0000'} icon="account-circle" />}
+      />
+      <ProgressBar style={styles.progressBar} progress={0.4} color={'#ff0000'} />
+      <ProgressBar style={styles.progressBar} progress={0.7} color={'#0000ff'} />
+
+      <List.Item
+        title="First Item"
+        description="Item description"
+        left={props => <List.Icon color={'#00ff00'} icon="account-circle" />}
+      />
+      <ProgressBar style={styles.progressBar} progress={0.9} color={'#00ff00'} />
+
+      <List.Item
+        title="First Item"
+        description="Item description"
+        left={props => <List.Icon color={'#0000ff'} icon="account-circle" />}
+      />
+      <ProgressBar style={styles.progressBar} progress={0.6} color={'#0000ff'} />
+
+
     </View>
   );
 }
@@ -14,7 +36,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  progressBar: {
+    height: 8,
+  }
 });
